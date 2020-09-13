fd40deaa45c1b70fb744c8cf7214e37400d121a8
Albert <yaaal@mail.ru>
2020-09-13 23:21:07 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Configuration;
+using System.Data;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OracleConnection con = null;
+        public MainWindow()
+        {
+            this.setConnection();
+            InitializeComponent();
+        }
+
+        private void setConnection()
+        {
+
+            OracleConnectionStringBuilder sb = new OracleConnectionStringBuilder();
+            sb.DataSource = "localhost:1521/orcl";
+            sb.DBAPrivilege = "SYSDBA";
+            sb.UserID = "SYS";
+            sb.Password = "3aPa3a19892811";
+
+            String connectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            con = new OracleConnection(connectionString);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception exp)
+            {
+
+            }
+        }
+
+        private string conStr()
+        {
+
+            OracleConnectionStringBuilder sb = new OracleConnectionStringBuilder();
+            sb.DataSource = "localhost:1521/orcl";
+            sb.DBAPrivilege = "SYSDBA";
+            sb.UserID = "SYS";
+            sb.Password = "3aPa3a19892811";
+            // {    DATA SOURCE=localhost:1521/orcl;USER ID = SYS;PASSWORD = 3aPa3a19892811;DBA PRIVILEGE = SYSDBA;}
+
+            OracleConnection conn = new OracleConnection(sb.ToString());
+            conn.Open();
+            return sb.ToString();
+        }
+
+        private void updateMyDataGrid() {
+
+
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "select * from city inner JOIN firm on city.city_id = firm.jur_city_id or city.city_id = firm.post_city_id WHERE UPPER(city.name) = UPPER('Миасс') or UPPER(firm.name) = UPPER('мир света') ORDER BY city.name";
+            //cmd.CommandText = d;
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            myDataGrid.ItemsSource = dt.DefaultView;
+            dr.Close();
+
+            //string queryString = "SELECT * FROM FIRM";
+            //using (OracleConnection connection = new OracleConnection(conStr()))
+            //{
+
+            //    OracleCommand command = new OracleCommand(queryString, connection);
+            //    connection.Open();
+            //    OracleDataReader reader = command.ExecuteReader();
+            //    DataTable dt = new DataTable();
+            //    dt.Load(reader);
+            //    myDataGrid.ItemsSource = dt.DefaultView;
+            //    reader.Close();
+            //}
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            updateMyDataGrid();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            con.Close();
+        }
+    }
+}
